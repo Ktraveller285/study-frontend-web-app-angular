ee45a340f5fea9136dc6c7da127e06391752dd7a
cerisier1407 <km002@k-movers.com>
2022-04-09 15:11:33 +0900

    NumOfCompletedTasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  tasks = [
+    {
+      name: 'ライブの申し込み',
+      isCompleted: false,
+    },
+    {
+      name: 'セトリの予想',
+      isCompleted: true,
+    },
+  ];
+
+  addTask(taskName: string) {
+    this.tasks.push({
+      name: taskName,
+      isCompleted: false,
+    });
+  }
+
+  getNumOfCompletedTasks() {
+    // 【課題 (タスクの完了数を返すようにしてください)】
+    let numOfCompletedTasks = 0;
+    for (let task of this.tasks) {
+      if (task.isCompleted == true) {
+        numOfCompletedTasks++;
+      }
+    }
+
+    return numOfCompletedTasks;
+  }
 }
