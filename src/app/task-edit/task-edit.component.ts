67003e9757cc0d85fe408140d77bfdb5ddb17ba0
cerisier1407 <km002@k-movers.com>
2022-04-16 14:56:12 +0900

    Add: タスク追加画面に移動した際にフォーカスが自動的にテキストボックスに移動するよう変更

--- src/app/task-edit/task-edit.component.ts
+++ src/app/task-edit/task-edit.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { TaskManagerService } from '../task-manager.service';
 
 @Component({
@@ -6,8 +13,20 @@ import { TaskManagerService } from '../task-manager.service';
   templateUrl: './task-edit.component.html',
   styleUrls: ['./task-edit.component.scss'],
 })
-export class TaskEditComponent implements OnInit {
-  constructor(public taskManager: TaskManagerService) {}
+export class TaskEditComponent implements OnInit, AfterViewInit {
+  @ViewChild('addTaskNameInput') taskNameInputElem!: ElementRef;
+
+  constructor(
+    public taskManager: TaskManagerService,
+    private changeDetectorRef: ChangeDetectorRef
+  ) {}
+
+  ngAfterViewInit(): void {
+    // フォーカスをあてる
+    this.taskNameInputElem.nativeElement.focus();
+    // Angularに強制的に変更検出させる
+    this.changeDetectorRef.detectChanges();
+  }
 
   ngOnInit(): void {}
 }
